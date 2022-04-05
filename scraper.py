c46bace3c2436dc8c7574948fe612f0dee31d0d8
Robin Gisler <63500142+gislerro@users.noreply.github.com>
2022-04-05 18:10:50 +0200

    small optimization

--- scraper.py
+++ scraper.py
@@ -12,7 +12,6 @@ url = 'https://rplace.space/combined/'
 def get_image_ids(start_id, end_id, granularity):
    res = requests.get(url)
    
-   start = 0
    ids = []
    if res.status_code == 200:
       soup = BeautifulSoup(res.text, 'html.parser')
@@ -20,17 +19,17 @@ def get_image_ids(start_id, end_id, granularity):
          id = os.path.splitext(a.get('href'))[0]
          try:
             id = int(id)
-            start = len(ids) if id == start_id else start
-            ids.append(id)
+            if id > end_id:
+               break
+            if id >= start_id:
+               ids.append(id)
          except ValueError: # skip <a> tags that aren't image links
             continue
-         if id == end_id:
-            break
    else: 
       print(f'Could not get image ids from {url}')
       sys.exit()
       
-   return ids[start::granularity]
+   return ids[::granularity]
 
 
 def get_image(id):
